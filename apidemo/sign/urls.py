a617e13d7c4ca28aff375a04c2a6ed8c1d33a24e
asfrank <asfrank@qq.com>
2019-06-16 22:43:19 +0800

    接口开发

--- apidemo/sign/urls.py
+++ apidemo/sign/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from sign import views_api
 
 urlpatterns = [
-    path('add_event/', views_api.add_event)
+    path('add_event/', views_api.add_event),
+    path('get_event_list', views_api.get_event_list),
+    path('user_sign', views_api.user_sign),
 ]
--- apidemo/sign/views_api.py
+++ apidemo/sign/views_api.py
@@ -1,7 +1,9 @@
-from django.core.exceptions import ValidationError
+import time
+from django.core.exceptions import ValidationError, ObjectDoesNotExist
 from django.http import JsonResponse
 
-from sign.models import Event
+from sign.models import Event, Guest
+
 
 #添加发布会接口
 def add_event(request):
@@ -35,3 +37,91 @@ def add_event(request):
     else:
         return JsonResponse({"status": 10031, "message": "请求方法错误"})
 
+
+#发布会查询
+def get_event_list(request):
+    eid = request.GET.get("eid", "")  # 发布会id
+    name = request.GET.get("name", "")  # 发布会名称
+
+    if eid == '' and name == '':
+        return JsonResponse({'status': 10021, 'message': '参数不能为空'})
+
+    if eid != '':
+        try:
+            int(eid)
+        except ValueError:
+            return JsonResponse({'status': 10025, 'message': 'eid类型错误'})
+        event = {}
+        try:
+            result = Event.objects.get(id=eid)
+        except ObjectDoesNotExist:
+            return JsonResponse({'status': 10022, 'message': '查询结果为空'})
+        else:
+            event['eid'] = result.id
+            event['name'] = result.name
+            event['limit'] = result.limit
+            event['status'] = result.status
+            event['address'] = result.address
+            event['start_time'] = result.start_time
+            return JsonResponse({'status': 200, 'message': 'success', 'data': event})
+
+    if name != '':
+        datas = []
+        results = Event.objects.filter(name__contains=name)
+        if results:
+            for r in results:
+                event = {}
+                event['eid'] = r.id
+                event['name'] = r.name
+                event['limit'] = r.limit
+                event['status'] = r.status
+                event['address'] = r.address
+                event['start_time'] = r.start_time
+                datas.append(event)
+            return JsonResponse({'status': 200, 'message': 'success', 'data': datas})
+        else:
+            return JsonResponse({'status': 10022, 'message': '查询结果为空'})
+
+#用户签到接口
+def user_sign(request):
+    eid = request.POST.get('eid', '')  # 发布会id
+    phone = request.POST.get('phone', '')  # 嘉宾手机号
+
+    if eid == '' or phone == '':
+        return JsonResponse({'status': 10021, 'message': 'parameter error'})
+
+    try:
+        result = Event.objects.get(id=eid)
+    except Event.DoesNotExist:
+        return JsonResponse({'status': 10022, 'message': 'event id null'})
+
+    if result.status is False:
+        return JsonResponse({'status': 10023, 'message': 'event status is not available'})
+
+    event_time = result.start_time  # 发布会时间
+    timeArray = time.strptime(str(event_time), "%Y-%m-%d %H:%M:%S")
+    e_time = int(time.mktime(timeArray))
+
+    n_time = int(str(time.time()).split(".")[0])  # 当前时间
+
+    if n_time >= e_time:
+        return JsonResponse({'status': 10024, 'message': 'event has started'})
+
+    result = Guest.objects.filter(phone=phone)
+    if not result:
+        return JsonResponse({'status': 10025, 'message': 'user phone null'})
+    else:
+        for res in result:
+            if res.event_id == int(eid):
+                break
+        else:
+            return JsonResponse({'status': 10026, 'message': 'user did not participate in the conference'})
+
+    result = Guest.objects.get(event_id=eid, phone=phone)
+    print(result.sign)
+    if result.sign is True:
+        return JsonResponse({'status': 10027, 'message': 'user has sign in'})
+    else:
+        result.sign = True
+        result.save()
+        return JsonResponse({'status': 200, 'message': 'sign success'})
